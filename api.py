208dc8878a729ea502f9ed8e12a1faeb2d0175fa
Charles <charles.lagace-leblanc.1@ulaval.ca>
2019-11-21 13:57:19 -0500

    modif_main_et_api

--- api.py
+++ api.py
@@ -1,1 +1,44 @@
+"""Modul API
+utilise requests pour faire des requètes sur le site du cours
+"""
+import requests
 
+URL_BASE = 'https://python.gel.ulaval.ca/quoridor/api/'
+
+def lister_parties(idul):
+    """Retourne la liste des 20 dernières paries"""
+    rep = requests.get(URL_BASE+'lister/', params={'idul': f'{idul}'})
+    if rep.status_code == 200:
+        rep = rep.json()
+        return rep['parties']
+    else:
+        print(f"Le GET sur {URL_BASE+'lister'} a produit le code d'erreur {rep.status_code}.")
+        raise RuntimeError
+
+def débuter_partie(idul):
+    """Demande au serveur de démarer une partie
+    Retourne le ID de la partie et son état initial
+    """
+    rep = requests.post(URL_BASE+'débuter/', data={'idul': f'{idul}'})
+    if rep.status_code == 200:
+        rep = rep.json()
+        return rep['id'], rep['état']
+    else:
+        print(f"Le GET sur {URL_BASE+'lister'} a produit le code d'erreur {rep.status_code}.")
+        raise RuntimeError
+
+def jouer_coup(id_partie, type_coup, position):
+    """Effectue le coup indiqué par l'utilisateur
+    Retourne l'état suite à la réponse de l'automate
+    """
+    rep = requests.post(URL_BASE+'jouer/', data={'id': f'{id_partie}', 'type': f'{type_coup}', 'pos': f'{position}'})
+    if rep.status_code == 200:
+        rep = rep.json()
+        if rep['gagnant']:
+            print(f"Le gagnant est {rep['gagnant']}")
+            raise StopIteration
+        else:
+            return rep['etat']
+    else:
+        print(f"Le GET sur {URL_BASE+'lister'} a produit le code d'erreur {rep.status_code}.")
+        raise RuntimeError
--- main.py
+++ main.py
@@ -1,1 +1,66 @@
+"""Module main"""
+import argparse
 
+def analyser_commande():
+    """Analyse les arguments de la commande d'exécution"""
+    parser = argparse.ArgumentParser()
+    # On joute le paramètre IDUL
+    parser.add_argument(
+        "idul", 
+        help="IDUL du joueur."
+    )
+    # On ajoute le paramètre optionnel --lister
+    parser.add_argument(
+        "-l", "--lister", dest="lister", action='store_true',
+        help="Lister les identifiants de vos 20 dernières parties."
+    )
+    return parser.parse_args()
+
+def afficher_damier_ascii(idul, etat):
+    """Affiche le damier à partir de l'état du jeu"""
+    # On créer un variable pour entreposer les charactère du damier
+    buffer = f"\nLégende: 1={idul}, 2=automate\n"
+    buffer += f"   -----------------------------------\n"
+    mat_line = []
+    mat_open = []
+    # On creer une liste contenant les caractères de base pour un 
+    # damier de jeu vide
+    for i in range(0, 9):
+        mat_line.append(list(f"{(9-i)} | .   .   .   .   .   .   .   .   . |\n"))
+    for i in range(0, 8):
+        mat_open.append(list("  |                                   |\n"))
+    #ajouter les pieces des joueurs dans le damier
+    pos_joueur = etat["joueurs"][0]["pos"]
+    pos_automate = etat["joueurs"][1]["pos"]
+    mat_line[9-pos_joueur[1]][4 + (pos_joueur[0] - 1)*4] = "1"
+    mat_line[9-pos_automate[1]][4 + (pos_automate[0] - 1)*4] = "2"
+    #ajouter les murs dans le damier
+    list_hor = etat['murs']['horizontaux']
+    list_ver = etat['murs']['verticaux']
+    for coord in list_hor:
+        y = 9-coord[1]
+        x = 4+(coord[0]-1)*4
+        mat_open[y][x-1] = '-'
+        mat_open[y][x] = '-'
+        mat_open[y][x+1] = '-'
+        mat_open[y][x+2] = '-'
+        mat_open[y][x+3] = '-'
+        mat_open[y][x+4] = '-'
+        mat_open[y][x+5] = '-'
+    for coord in list_ver:
+        y = y = 9-coord[1]
+        x = 4+(coord[0]-1)*4
+        mat_line[y][x-2] = '|'
+        mat_open[y-1][x-2] = '|'
+        mat_line[y-1][x-2] = '|'
+    for i in range(len(mat_line)):
+        buffer += ''.join(mat_line[i])
+        if i < len(mat_open):
+            buffer += ''.join(mat_open[i])
+    buffer += "--|-----------------------------------\n"
+    buffer += "  | 1   2   3   4   5   6   7   8   9\n"
+    print(buffer)
+
+if __name__ == "__main__":
+    # On appèle analyser_commande() au démarage pour lire les arguments
+    ARGS = analyser_commande()
